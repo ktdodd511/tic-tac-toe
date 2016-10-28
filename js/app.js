0eec322b7bec0dc83a885a86eaf1910813199aed
ktdodd511 <katiedodd@Katies-MacBook-Air.local>
2016-10-28 14:54:35 -0700

    created reset button that works, set up board

--- js/app.js
+++ js/app.js
@@ -2,5 +2,16 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  console.log('js file is connected');
 
+
+  box1 = $('#box1');
+
+  box1.click(function() {
+    alert("it works!");
+  });
+
+$('#reset').click(function() {
+  location.reload();
 });
+
