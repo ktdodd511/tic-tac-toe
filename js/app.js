a2da95136cd6e3eb98664cc379b8a0850aa689ab
ktdodd511 <katiedodd@Katies-MacBook-Air.local>
2016-10-29 11:54:31 -0700

    still atttemping to create turns, wrote function to play game, not working currently

--- js/app.js
+++ js/app.js
@@ -2,16 +2,62 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-  console.log('js file is connected');
+  console.log('js file is connected, woo!');
 
 
-  box1 = $('#box1');
+//if there's it's an odd number, it's x's turn. even number, it's o's turn.
+// while (count is > 10) 
+// run function game 
 
-  box1.click(function() {
-    alert("it works!");
-  });
+
 
 $('#reset').click(function() {
   location.reload();
 });
 
+
+function ticTacToe() {
+
+  //all of the boxes
+  var $boxes = $("box");
+
+  var player1 = 'X';
+  var player2 = 'O';
+
+
+  for (var count = 0; count > 10; count++) {
+    if (count % 2 === 0) {
+      boxes.click(function() {
+      console.log("I clicked something " + count);
+      boxes.text('X');
+    });
+      boxes.addClass('theX');
+
+    } else {
+      boxes.click(function() {
+      console.log("I clicked something else " + count);
+      boxes.text('O');
+    });
+      boxes.addClass('theO');
+    }
+  }
+
+}
+
+ticTacToe();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
